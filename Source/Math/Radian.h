784b04d6cbb83932cd43ee4c0a93e47efc2a6977
Manuel Riecke <spell1337@gmail.com>
2013-07-09 00:27:01 +0200

    Radian update.

--- Source/Math/Radian.h
+++ Source/Math/Radian.h
@@ -5,8 +5,10 @@
 
 #pragma once
 
-//! @ingroup Angle
-//! @{
+/** @ingroup Angle
+ * The Radian class is a lightweight wrapper around the \a float datatype used by low level functions like \ref Sin and \ref Cos.
+ * It's purpose is to denote the difference between a arbitary floating number and a \ref Radian so that classes like \ref Angle can convert accordingly.
+*/
 struct Radian
 {
 public:
@@ -15,9 +17,10 @@ public:
 	operator float&(){ return radians; };
 	constexpr operator const float&() const { return radians; };
 private:
-	float radians=0.f;
+	float radians = 0.f;
 };
-//! @}
 
-extern const Radian PI;
-extern const Radian Tau;
+//! A half turn in radians. Equal to the ratio of a circle's circumference to it's diameter.
+constexpr Radian PI  = 3.14159;
+//! A full turn in radians. Equal to the ratio of a circle's circumference to it's radius.
+constexpr Radian Tau = PI * 2;
