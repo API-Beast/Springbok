314bee711215d8d1deb58a9472ada88cbda5cd62
Beast <spell1337@gmail.com>
2014-03-08 17:07:26 +0100

    Include debug utils from WebGL branch.

---
+++ Source/Utils/Debug.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <iostream>
+
+namespace Debug 
+{
+	void Write(const char* message);
+
+	template<typename T, typename... Args> void Write(const char* message, T value, Args... args)
+	{	
+		if (message[0] == '\0')
+			return;
+		while (*message) 
+		{
+			if (*message == '$') 
+			{
+				if (*(message + 1) == '$') 
+				{
+					++message;
+				}
+				else
+				{
+					std::cout << value;
+					char next = (message + 1)[0];
+					if (next == '\0')
+						std::cout << std::endl;
+ 					else Write(message + 1, args...); 
+					return;
+				}
+			}
+			std::cout << *message++;
+		}
+		std::cout << "\nWARNING: extra arguments provided to Debug::Write()" << std::endl;
+	}
+}
