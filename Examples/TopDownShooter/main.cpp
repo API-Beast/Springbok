1565cb57ca9152cd8535a31e6aa305d5617b5f04
Beast <spell1337@gmail.com>
2014-03-16 17:49:37 +0100

    Remove old examples.

--- Examples/TopDownShooter/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "TopDownShooter.h"
-#include <Source/Resources/ResourceManager.h>
-
-int main()
-{
-	ResourceManager::GetInstance()->findPrimaryResourcePath({"./Assets"});
-	TopDownShooter game;
-	return game.execute();
-}
